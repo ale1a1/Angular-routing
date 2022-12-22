cabf754ac10dc743b4dc2d4d9f8746e02de33b71
alessandro ladu <ale1a@libero.it>
2022-12-22 17:05:46 +0000

    setting up nested routes 2

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -20,7 +20,10 @@ export class EditServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // let id = this.route.snapshot.params['id'];
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
